69e8f0470675b1f347b3d970c8bbbab564a28c7e
italomatiastd <italogoncalvees@gmail.com>
2025-06-19 15:08:36 -0300

    feat: Adiciona método de pesquisa de produtos por nome, descrição, categoria e preço

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -20,6 +20,35 @@ const ProductService = {
     deleteProduct: async (id: string): Promise<IProduct | null> => {
         return await Product.findByIdAndDelete(id);
     },
+    searchProducts: async (params: {  // Ajustar searchProducts depois de fazer as categorias
+        name?: string;       // Busca por nome (parcial e case-insensitive)
+        category?: string;   // Busca por categoria (exata)
+        minPrice?: number;   // Preço mínimo
+        maxPrice?: number;   // Preço máximo
+    }): Promise<IProduct[]> => {
+        const filter: any = {}
+
+        if(params.name) {
+            filter.name = { $regex: params.name, $options: 'i'}
+        }  
+        if(params.category) {
+            filter.category = { $regex: params.category, $options: 'i'}
+        }
+        if(params.minPrice || params.maxPrice) {
+            filter.price = {};
+            if(params.minPrice) {
+                filter.price.$gte = params.minPrice
+            }
+            if(params.maxPrice) {
+                filter.price.$lte = params.maxPrice
+            }
+        }
+
+        return await Product.find(filter) as IProduct[];
+    }
+     
 }
+
+
 
 export default ProductService;
