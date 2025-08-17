c72a44da2b6fafba9bcd22b543d8056ecc120ad9
Mickael <65185697+mickaelluis@users.noreply.github.com>
2025-08-17 00:47:11 -0300

    chore(product): melhorar controller de produtos
    - padroniza retornos HTTP (200/201/400/404/500)
    - adiciona logging em erros inesperados
    - valida cast/validation errors para respostas mais claras
    ```chore(product): melhorar controller de produtos
    
    - padroniza retornos HTTP (200/201/400/404/500)
    - adiciona logging em erros inesperados
    - valida cast/validation errors para respostas mais claras

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,94 +1,99 @@
-import User from '../models/user.model';
-require('dotenv').config();
-const jwt = require('jsonwebtoken')
-const secret = process.env.JWT_TOKEN
-import { IUser } from '../models/user.model';
-import  clientes, { IClients } from '../models/Clientes.model';
-import { Message } from 'twilio/lib/twiml/MessagingResponse';
+import User from "../models/user.model";
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWT_TOKEN;
+import { IUser } from "../models/user.model";
+import clientes, { IClients } from "../models/Clientes.model";
+import { Message } from "twilio/lib/twiml/MessagingResponse";
 
 const AuthService = {
-    register: async (body: { name: string; email: string; password: string }) => {
-       try {
-            // Validação simples dos dados de entrada
-            const existingUser= await User.findOne({ email: body.email });
-            if (existingUser) { 
-                return {
-                    status: 400,
-                    message: 'Usuário já existe.',
-                };
-            }
-            const { name, email, password } = body;
-            const user = new User({ name, email, password });
-            await user.save();
-            const Token = await jwt.sign( {body: email}, secret, { expiresIn: '1d' });
-            //console.log(user)
-            const novoCliente = await clientes.create({userid: user.id});
-            console.log(novoCliente.errors )
+  register: async (body: { name: string; email: string; password: string }) => {
+    try {
+      // Validação simples dos dados de entrada
+      const existingUser = await User.findOne({ email: body.email });
+      if (existingUser) {
+        return {
+          status: 400,
+          message: "Usuário já existe.",
+        };
+      }
+      const { name, email, password } = body;
+      const user = new User({ name, email, password });
+      await user.save();
+      const Token = await jwt.sign({ body: email }, secret, {
+        expiresIn: "1d",
+      });
+      //console.log(user)
+      const novoCliente = await clientes.create({ userid: user.id });
+      console.log(novoCliente.errors);
 
-            if( novoCliente.errors ){
-                console.log('An error occurred while creating the client')
-                await user.deleteOne({_id: user.id})
-                    return {
-                        status: 401,
-                        message: 'Server error'
-                    } 
-            }
+      if (novoCliente.errors) {
+        console.log("An error occurred while creating the client");
+        await user.deleteOne({ _id: user.id });
+        return {
+          status: 401,
+          message: "Server error",
+        };
+      }
 
-            if ( !novoCliente ){
-                 console.log('An error occurred while creating the client')
-                await user.deleteOne({_id: user.id})
-                    return {
-                        status: 401,
-                        message: 'Server error'
-                    } 
-            }; 
-            // Gera um token JWT para o usuário registrado
-            return {
-                status: 201,
-                data: { 'Token': Token}
-            }; 
-            
-        } catch (error) {
-            const {  email } = body;
-            const userToDelete = await User.findOne({ email: email });
-            if (userToDelete) {
-                    await userToDelete.deleteOne({ email: email });
-                    console.log("Usuário órfão deletado com sucesso.");
-             }
-            console.error('Error registering user:', error); 
-            // Retorna um erro genérico em caso de falha
-            return {
-                status: 500,
-                message: 'Internal server error.',
-            };
-        } 
-    },
-
-    login: async (body: {  email: string; password: string }) => {
-        try {
-            const user = await User.findOne({ email: body.email}) as IUser | null;
-            const token = await jwt.sign( { email: body.email}, secret, { expiresIn: '1d' });
-            if (user && await user.comparePassword(body.password)){ //valida se a senha e o mail sao validos 
-                return { // devolve o token no padrão Auth Type
-                    status: 200,
-                    Authorization: `Bearer ${token}` 
-                }
-            }else {// Retorna um erro em caso de senha ou email incorreto                 
-                    return {
-                        status: 401,
-                        message: 'E-mail ou senha incorretos.',
-                     }
-                }
-         } catch (error) {
-             console.error('Error registering user:', error); 
-            // Retorna um erro genérico em caso de falha
-            return {
-                status: 500,
-                message: 'Internal server error.',
-            };
-        }
-        
+      if (!novoCliente) {
+        console.log("An error occurred while creating the client");
+        await user.deleteOne({ _id: user.id });
+        return {
+          status: 401,
+          message: "Server error",
+        };
+      }
+      // Gera um token JWT para o usuário registrado
+      return {
+        status: 201,
+        data: { Token: Token },
+      };
+    } catch (error) {
+      const { email } = body;
+      const userToDelete = await User.findOne({ email: email });
+      if (userToDelete) {
+        await userToDelete.deleteOne({ email: email });
+        console.log("Usuário órfão deletado com sucesso.");
+      }
+      console.error("Error registering user:", error);
+      // Retorna um erro genérico em caso de falha
+      return {
+        status: 500,
+        message: "Internal server error.",
+      };
     }
-} 
+  },
 
-export default AuthService; 
+  login: async (body: { email: string; password: string }) => {
+    try {
+      const user = (await User.findOne({ email: body.email })) as IUser | null;
+      const token = await jwt.sign({ email: body.email }, secret, {
+        expiresIn: "1d",
+      });
+      if (user && (await user.comparePassword(body.password))) {
+        //valida se a senha e o mail sao validos
+        return {
+          // devolve o token no padrão Auth Type
+          status: 200,
+          Authorization: `Bearer ${token}`,
+        };
+      } else {
+        // Retorna um erro em caso de senha ou email incorreto
+        return {
+          status: 401,
+          message: "E-mail ou senha incorretos.",
+        };
+      }
+    } catch (error) {
+      console.error("Error registering user:", error);
+      // Retorna um erro genérico em caso de falha
+      return {
+        status: 500,
+        message: "Internal server error.",
+      };
+    }
+  },
+};
+
+export default AuthService;
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,25 +1,28 @@
 // Métodos pra criar:
 // getAllProducts , getProductById --> clique, searchProducts, createProduct, updateProduct, deleteProduct
 
-import  Product, { IProduct } from '../models/product.model';
+import Product, { IProduct } from "../models/product.model";
 
 const ProductService = {
-    getAllProducts: async (): Promise<IProduct[]> => {
-        return await Product.find();
-    },
-    getProductById: async (id: string): Promise<IProduct | null> => {
-        return await Product.findById(id);
-    },
-    createProduct: async (productData: Partial<IProduct>): Promise<IProduct> => {
-        const newProduct = new Product(productData);
-        return await newProduct.save();
-    },
-    updateProduct: async (id: string, productData: Partial<IProduct>): Promise<IProduct | null> => {
-        return await Product.findByIdAndUpdate(id, productData, { new: true});
-    },
-    deleteProduct: async (id: string): Promise<IProduct | null> => {
-        return await Product.findByIdAndDelete(id);
-    },
-}
+  getAllProducts: async (): Promise<IProduct[]> => {
+    return await Product.find();
+  },
+  getProductById: async (id: string): Promise<IProduct | null> => {
+    return await Product.findById(id);
+  },
+  createProduct: async (productData: Partial<IProduct>): Promise<IProduct> => {
+    const newProduct = new Product(productData);
+    return await newProduct.save();
+  },
+  updateProduct: async (
+    id: string,
+    productData: Partial<IProduct>
+  ): Promise<IProduct | null> => {
+    return await Product.findByIdAndUpdate(id, productData, { new: true });
+  },
+  deleteProduct: async (id: string): Promise<IProduct | null> => {
+    return await Product.findByIdAndDelete(id);
+  },
+};
 
 export default ProductService;
